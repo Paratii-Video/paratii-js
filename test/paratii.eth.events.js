55743d66e08479df978063ebb6db5783e5dd80a8
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-01-05 17:51:00 +0100

    fix

--- test/paratii.eth.events.js
+++ test/paratii.eth.events.js
@@ -1,9 +1,10 @@
 import { Paratii } from '../lib/paratii.js'
-import { address, privateKey, address1 } from './utils.js'
+import { address, privateKey, address17 } from './utils.js'
 import { assert } from 'chai'
 
 describe('paratii.eth API: :', function () {
   let paratii
+
   beforeEach(async function () {
     paratii = await new Paratii({
       provider: 'http://localhost:8545/rpc/',
@@ -11,14 +12,13 @@ describe('paratii.eth API: :', function () {
       privateKey: privateKey
     })
     await paratii.eth.deployContracts()
+    paratii.eth.web3.setProvider('ws://localhost:8546/rpc/')
   })
 
   it('subscription to Tranfer PTI events should work as expected', async function () {
-    paratii.eth.web3.setProvider('ws://localhost:8546/rpc/')
-
+    let beneficiary = address17
     let ptiTransfer = paratii.eth.events.addListener('transfer')
 
-    let beneficiary = address1
     let amount = paratii.eth.web3.utils.toWei('4', 'ether')
     await paratii.eth.transfer(beneficiary, amount, 'PTI')
 
