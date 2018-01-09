24047a2aec0a80489fc7d4ffabca6f81f0fa455f
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-01-09 23:31:33 +0100

    contract events

--- test/paratii.eth.events.js
+++ test/paratii.eth.events.js
@@ -1,5 +1,5 @@
 import { Paratii } from '../lib/paratii.js'
-import { address, privateKey } from './utils.js'
+import { address, privateKey, address1 } from './utils.js'
 import { assert } from 'chai'
 
 describe('paratii.eth.events API: :', function () {
@@ -12,37 +12,36 @@ describe('paratii.eth.events API: :', function () {
       privateKey: privateKey
     })
     await paratii.eth.deployContracts()
+    paratii.eth.web3.setProvider('ws://localhost:8546')
   })
 
   it('subscription to Tranfer PTI events should work as expected', function (done) {
-    paratii.eth.web3.setProvider('ws://localhost:8546/rpc/')
     let beneficiary = '0xDbC8232Bd8DEfCbc034a0303dd3f0Cf41d1a55Cf'
     let amount = paratii.eth.web3.utils.toWei('4', 'ether')
 
     let ptiTransferData = paratii.eth.events.addListener('TransferPTI', function (log) {
-      const received = paratii.eth.web3.utils.hexToNumberString(log.returnValues.value)
+      const received = log.returnValues.value
       assert.equal(received, amount)
       done()
       ptiTransferData.unsubscribe()
     })
 
-    // console.log(ptiTransferData)
-
     paratii.eth.transfer(beneficiary, amount, 'PTI')
   })
 
-  it.skip('subscription to Create Video events should work as expected', function () {
-    // let videoCreate = paratii.eth.events.addListener('logs')
-    let creator = '0xDbC8232Bd8DEfCbc034a0303dd3f0Cf41d1a55Cf'
+  it('subscription to Create Video events should work as expected', function (done) {
+    let creator = address1
     let price = 3 * 10 ** 18
     let ipfsHash = 'xyz'
     let ipfsData = 'zzz'
     let videoId = 'some-id'
 
-    // let logCreateVideoData = paratii.eth.events.addListener('LogCreateVideo', function (log) {
-    //   console.log(log)
-    //   done()
-    // })
+    let logCreateVideoData = paratii.eth.events.addListener('LogCreateVideo', function (log) {
+      const receivedVideoId = log.returnValues.videoId
+      assert.equal(videoId, receivedVideoId)
+      done()
+      logCreateVideoData.unsubscribe()
+    })
 
     paratii.eth.vids.create({
       id: videoId,
