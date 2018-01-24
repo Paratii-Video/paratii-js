4286ced4e81a09913b81cace59f5b20d1d52a2f6
Yahya <ya7yaz@gmail.com>
2018-01-24 21:42:52 +0200

    commenting out unused code till protocol upgrades

--- dist/paratii.ipfs.uploader.js
+++ dist/paratii.ipfs.uploader.js
@@ -485,75 +485,70 @@ var Uploader = function (_EventEmitter) {
     //     this._signalTranscoderPull(callback)
     //   )
     // }
+    //
+    // _signalTranscoderPull (callback) {
+    //   return pull.collect((err, res) => {
+    //     if (err) {
+    //       return callback(err)
+    //     }
+    //     const file = res[0]
+    //     console.log('Adding %s finished', file.path)
+    //
+    //     // statusEl.innerHTML += `Added ${file.path} as ${file.hash} ` + '<br>'
+    //     // Trigger paratii transcoder signal
+    //     this.signalTrancoder(file, callback)
+    //   })
+    // }
+    //
+    // signalTranscoder (file, callback) {
+    //   let msg = this._ipfs.protocol.createCommand('transcode', {hash: file.hash, author: this.id.id})
+    //   this._node.swarm.connect('/dns4/bootstrap.paratii.video/tcp/443/wss/ipfs/QmeUmy6UtuEs91TH6bKnfuU1Yvp63CkZJWm624MjBEBazW', (err, success) => {
+    //     if (err) throw err
+    //     this._node.swarm.peers((err, peers) => {
+    //       console.log('peers: ', peers)
+    //       if (err) throw err
+    //       peers.map((peer) => {
+    //         console.log('sending transcode msg to ', peer.peer.id.toB58String())
+    //         this._ipfs.protocol.network.sendMessage(peer.peer.id, msg, (err) => {
+    //           if (err) console.warn('[Paratii-protocol] Error ', err)
+    //         })
+    //
+    //         if (peer.addr) {
+    //         }
+    //       })
+    //       callback(null, file)
+    //     })
+    //   })
+    //     // paratii transcoder signal.
+    //   this._ipfs.protocol.notifications.on('command', (peerId, command) => {
+    //     console.log('paratii protocol: Got Command ', command)
+    //     if (command.payload.toString() === 'transcoding:done') {
+    //       let args = JSON.parse(command.args.toString())
+    //       let result = JSON.parse(args.result)
+    //       console.log('args: ', args)
+    //       console.log('result: ', result)
+    //         // statusEl.innerHTML += `Video HLS link: /ipfs/${result.master.hash}\n`
+    //
+    //         // titleEl = document.querySelector('#input-title')
+    //         // console.log('titleEl: ', titleEl)
+    //       //   Meteor.call('videos.create', {
+    //       //     id: String(Math.random()).split('.')[1],
+    //       //     title: titleEl.value,
+    //       //     price: 0.0,
+    //       //     src: '/ipfs/' + result.master.hash,
+    //       //     mimetype: 'video/mp4',
+    //       //     stats: {
+    //       //       likes: 0,
+    //       //       dislikes: 0
+    //       //     }}, (err, videoId) => {
+    //       //       if (err) throw err
+    //       //       console.log('[upload] Video Uploaded: ', videoId)
+    //       //       statusEl.innerHTML += '\n Video Uploaded go to <b><a href="/play/' + videoId + '">/play/' + videoId + '</a></b>\n'
+    //       //     })
+    //     }
+    //   })
+    // }
 
-  }, {
-    key: '_signalTranscoderPull',
-    value: function _signalTranscoderPull(callback) {
-      var _this6 = this;
-
-      return pull.collect(function (err, res) {
-        if (err) {
-          return callback(err);
-        }
-        var file = res[0];
-        console.log('Adding %s finished', file.path);
-
-        // statusEl.innerHTML += `Added ${file.path} as ${file.hash} ` + '<br>'
-        // Trigger paratii transcoder signal
-        _this6.signalTrancoder(file, callback);
-      });
-    }
-  }, {
-    key: 'signalTranscoder',
-    value: function signalTranscoder(file, callback) {
-      var _this7 = this;
-
-      var msg = this._ipfs.protocol.createCommand('transcode', { hash: file.hash, author: this.id.id });
-      this._node.swarm.connect('/dns4/bootstrap.paratii.video/tcp/443/wss/ipfs/QmeUmy6UtuEs91TH6bKnfuU1Yvp63CkZJWm624MjBEBazW', function (err, success) {
-        if (err) throw err;
-        _this7._node.swarm.peers(function (err, peers) {
-          console.log('peers: ', peers);
-          if (err) throw err;
-          peers.map(function (peer) {
-            console.log('sending transcode msg to ', peer.peer.id.toB58String());
-            _this7._ipfs.protocol.network.sendMessage(peer.peer.id, msg, function (err) {
-              if (err) console.warn('[Paratii-protocol] Error ', err);
-            });
-
-            if (peer.addr) {}
-          });
-          callback(null, file);
-        });
-      });
-      // paratii transcoder signal.
-      this._ipfs.protocol.notifications.on('command', function (peerId, command) {
-        console.log('paratii protocol: Got Command ', command);
-        if (command.payload.toString() === 'transcoding:done') {
-          var args = JSON.parse(command.args.toString());
-          var result = JSON.parse(args.result);
-          console.log('args: ', args);
-          console.log('result: ', result);
-          // statusEl.innerHTML += `Video HLS link: /ipfs/${result.master.hash}\n`
-
-          // titleEl = document.querySelector('#input-title')
-          // console.log('titleEl: ', titleEl)
-          //   Meteor.call('videos.create', {
-          //     id: String(Math.random()).split('.')[1],
-          //     title: titleEl.value,
-          //     price: 0.0,
-          //     src: '/ipfs/' + result.master.hash,
-          //     mimetype: 'video/mp4',
-          //     stats: {
-          //       likes: 0,
-          //       dislikes: 0
-          //     }}, (err, videoId) => {
-          //       if (err) throw err
-          //       console.log('[upload] Video Uploaded: ', videoId)
-          //       statusEl.innerHTML += '\n Video Uploaded go to <b><a href="/play/' + videoId + '">/play/' + videoId + '</a></b>\n'
-          //     })
-        }
-      });
-    }
   }]);
   return Uploader;
 }(EventEmitter);
