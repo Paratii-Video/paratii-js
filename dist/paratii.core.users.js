f30d1084673f9d958c0ee226f377e762b0da6bf3
Milan Kragujević <milankragujevic@gmail.com>
2018-02-07 21:09:57 +0100

    add tests for core.users, #55

--- dist/paratii.core.users.js
+++ dist/paratii.core.users.js
@@ -9,6 +9,10 @@ var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
 
 var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var dopts = require('default-options');
@@ -18,12 +22,32 @@ var dopts = require('default-options');
  *
  */
 
-var ParatiiCoreUsers = exports.ParatiiCoreUsers = function ParatiiCoreUsers(config) {
-  (0, _classCallCheck3.default)(this, ParatiiCoreUsers);
+var ParatiiCoreUsers = exports.ParatiiCoreUsers = function () {
+  function ParatiiCoreUsers(config) {
+    (0, _classCallCheck3.default)(this, ParatiiCoreUsers);
 
-  var defaults = {
-    'db.provider': null
-  };
-  var options = dopts(config, defaults, { allowUnknown: true });
-  this.config = options;
-};
+    var defaults = {
+      'db.provider': null
+    };
+    var options = dopts(config, defaults, { allowUnknown: true });
+    this.config = options;
+  }
+
+  (0, _createClass3.default)(ParatiiCoreUsers, [{
+    key: 'create',
+    value: function create(options) {
+      return this.paratii.eth.users.create(options);
+    }
+  }, {
+    key: 'get',
+    value: function get(id) {
+      return this.paratii.eth.users.get(id);
+    }
+  }, {
+    key: 'update',
+    value: function update(id, options) {
+      return this.paratii.eth.users.update(id, options);
+    }
+  }]);
+  return ParatiiCoreUsers;
+}();
--- test/paratii.core.js
+++ test/paratii.core.js
@@ -4,11 +4,19 @@ import { assert } from 'chai'
 
 describe('paratii.core API: :', function () {
   let paratii, paratiiCore
-  beforeEach(function () {
-    paratii = new Paratii({
+  let userId = address
+  let userData = {
+    id: userId,
+    name: 'Humbert Humbert',
+    email: 'humbert@humbert.ru',
+    ipfsHash: 'some-hash'
+  }
+  beforeEach(async function () {
+    paratii = await new Paratii({
       address: address,
       privateKey: privateKey
     })
+    await paratii.eth.deployContracts()
     paratiiCore = paratii.core
   })
 
@@ -18,9 +26,21 @@ describe('paratii.core API: :', function () {
     assert.isOk(paratiiCore.vids)
   })
 
-  it.skip('core.users.create() should work as expected', async function () {
+  it('core.users.create() should work as expected', async function () {
+    let result = await paratii.eth.users.create(userData)
+    assert.equal(result, userId)
   })
 
-  it.skip('core.users.get() should work as expected', async function () {
+  it('core.users.get() should work as expected', async function () {
+    await paratii.eth.users.create(userData)
+    let user = await paratii.eth.users.get(userId)
+    assert.deepEqual(user, userData)
+  })
+
+  it('core.users.update() should work as expected', async function () {
+    await paratii.eth.users.create(userData)
+    await paratii.eth.users.update(userId, {ipfsHash: 'new-hash'})
+    let user = await paratii.eth.users.get(userId)
+    assert.equal(user.ipfsHash, 'new-hash')
   })
 })
