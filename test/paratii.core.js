7149195d41f140ac9b0f94dceda21f571576732c
Milan Kragujević <milankragujevic@gmail.com>
2018-02-08 18:04:39 +0100

    remove redundant tests in paratii.core

--- test/paratii.core.js
+++ test/paratii.core.js
@@ -4,13 +4,6 @@ import { assert } from 'chai'
 
 describe('paratii.core API: :', function () {
   let paratii, paratiiCore
-  let userId = address
-  let userData = {
-    id: userId,
-    name: 'Humbert Humbert',
-    email: 'humbert@humbert.ru',
-    ipfsHash: 'some-hash'
-  }
   beforeEach(async function () {
     paratii = await new Paratii({
       address: address,
@@ -24,23 +17,5 @@ describe('paratii.core API: :', function () {
     assert.isOk(paratiiCore)
     assert.isOk(paratiiCore.users)
     assert.isOk(paratiiCore.vids)
-  })
-
-  it('core.users.create() should work as expected', async function () {
-    let result = await paratii.eth.users.create(userData)
-    assert.equal(result, userId)
-  })
-
-  it('core.users.get() should work as expected', async function () {
-    await paratii.eth.users.create(userData)
-    let user = await paratii.eth.users.get(userId)
-    assert.deepEqual(user, userData)
-  })
-
-  it('core.users.update() should work as expected', async function () {
-    await paratii.eth.users.create(userData)
-    await paratii.eth.users.update(userId, {ipfsHash: 'new-hash'})
-    let user = await paratii.eth.users.get(userId)
-    assert.equal(user.ipfsHash, 'new-hash')
   })
 })
