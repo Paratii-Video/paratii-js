f9c01d7c3b41af721ee35760a24813f49346a015
Milan Kragujević <milankragujevic@gmail.com>
2018-02-09 15:13:27 +0000

    fixes of vouchers

--- test/paratii.eth.vouchers.js
+++ test/paratii.eth.vouchers.js
@@ -15,22 +15,32 @@ describe('paratii.eth.vouchers:', function () {
     let token = await paratii.eth.getContract('ParatiiToken')
     let vouchers = await paratii.eth.getContract('Vouchers')
     let addressOfVouchers = vouchers.options.address
-    await token.methods.transfer(addressOfVouchers, voucherAmountInitial11)
+    await token.methods.transfer(addressOfVouchers, voucherAmountInitial11).send()
   })
 
-  it('vouchers.add() should work as expected', async function () {
-    let result = await paratii.eth.vouchers.add({ voucherCode: voucherCode11, amount: voucherAmount11 })
+  it('vouchers.create() should work as expected', async function () {
+    let result = await paratii.eth.vouchers.create({ voucherCode: voucherCode11, amount: voucherAmount11 })
     assert.equal(hashedVoucherCode11, result)
   })
 
   it('vouchers.redeem() should work as expected', async function () {
-    let newVoucher = await paratii.eth.vouchers.add({ voucherCode: voucherCode11, amount: voucherAmount11 })
+    let newVoucher = await paratii.eth.vouchers.create({ voucherCode: voucherCode11, amount: voucherAmount11 })
     assert.isOk(paratii.eth.vouchers.redeem(newVoucher))
   })
 
   it('vouchers.redeem() should fail on used code', async function () {
-    let newVoucher = await paratii.eth.vouchers.add({ voucherCode: voucherCode11, amount: voucherAmount11 })
+    let newVoucher = await paratii.eth.vouchers.create({ voucherCode: voucherCode11, amount: voucherAmount11 })
     await paratii.eth.vouchers.redeem(newVoucher)
     assert.isNotOk(await paratii.eth.vouchers.redeem(newVoucher))
+  })
+
+  it('vouchers.create() should fail on invalid data', async function () {
+    await assert.isRejected(paratii.eth.vouchers.create({ voucherCode: null, amount: voucherAmount11 }))
+    await assert.isRejected(paratii.eth.vouchers.create({ voucherCode: 123, amount: voucherAmount11 }))
+    await assert.isRejected(paratii.eth.vouchers.create({ voucherCode: voucherCode11, amount: 'test' }))
+    await assert.isRejected(paratii.eth.vouchers.create({ voucherCode: voucherCode11, amount: 0 }))
+  })
+
+  it.skip('vouchers.create() should throw meaningful errors on failure', async function () {
   })
 })
