9822628df1094f7d66c6fa01d761fe72f36413b9
M Yahya <ya7yaz@gmail.com>
2018-07-29 19:41:28 +0300

    WIP: mine a block till a certain timestamp

--- test/utils.js
+++ test/utils.js
@@ -343,6 +343,33 @@ async function revealVoteFromDifferentAccount (privateKey, pollID, vote, salt, p
   return revealTx
 }
 
+async function createDummyVideo (paratii) {
+  let tx = await paratii.eth.vids.create({
+    id: 'dummy_vid',
+    owner: paratii.eth.getAccount(),
+    ipfsData: 'QmDummy'
+  })
+  return tx
+}
+
+async function mineABlock (paratii) {
+  // let tx = await paratii.eth.transfer('0x0', '0x01', 'ETH', 'dummy_tx')
+  // return tx
+  let tx
+  try {
+    tx = await paratii.eth.vids.get('dummy_vid')
+  } catch (e) {
+    if (e) {
+      console.log('no video was created')
+      tx = await createDummyVideo(paratii)
+    }
+  }
+
+  tx = await paratii.eth.vids.update('dummy_vid', {price: Math.floor(Math.random() * 10)})
+  tx = await paratii.eth.web3.eth.getBlock('latest')
+  return tx
+}
+
 export {
   address,
   address1,
@@ -364,5 +391,6 @@ export {
   testAccount,
   challengeFromDifferentAccount,
   voteFromDifferentAccount,
-  revealVoteFromDifferentAccount
+  revealVoteFromDifferentAccount,
+  mineABlock
 }
